4a7d4915a34e7e53b91d249ea137abca1476e2c0
mnorbury <martin.norbury@gmail.com>
2014-08-01 14:48:23 -0700

    Tidy up and wiring in variables.

--- src/application.py
+++ src/application.py
@@ -5,6 +5,8 @@ The main application file.
 """
 import logging
 import signal
+import argparse
+
 _LOGGER = logging.getLogger(__name__)
 
 from controller import Controller
@@ -14,16 +16,19 @@ from outputsource import Blink1Indicator
 
 if __name__ == '__main__':
 
+    parser = argparse.ArgumentParser(description="Jenkins build monitor.")
+    parser.add_argument('-build_host', default='buildsba:8085')
+    parser.add_argument('-loop_period', default=0.1)
+    parser.add_argument('-decay_period', default=30)
+    parser.add_argument('-log_level', default='info')
+    arguments = parser.parse_args()
+
     # Configurable inputs
-    buildhost = 'buildsba:8085'
-    loop_period = 0.1
-    decay_period = 30
-    level = getattr(logging, 'debug'.upper())
-    logging.basicConfig(level=level, format='%(asctime)-15s %(message)s')
+    logging.basicConfig(level=(getattr(logging, arguments.log_level.upper())), format='%(asctime)-15s %(message)s')
 
     # Build controller
-    input_source = JenkinsDataSource(buildhost)
-    output_source = Blink1Indicator()
+    input_source = JenkinsDataSource(arguments.build_host)
+    output_source = Blink1Indicator(arguments.loop_period)
     controller = Controller(input_source, output_source)
 
     # Attach ctrl-c handler
--- test/test_pattern.py
+++ test/test_pattern.py
@@ -1,30 +1,33 @@
-'''
+"""
 test_pattern.py - Test cases for the pattern module.
 
 :author: Martin Norbury
-'''
+"""
 from nose.tools import eq_
 
 from pattern import pulse_rgb
 
+
 class TestPattern(object):
 
     def __init__(self):
-        self.pulse_generator = pulse_rgb((255,0,0), 0.1)
+        self.pulse_generator = pulse_rgb((255, 0, 0), 0.1)
 
     def test_first_value_is_target(self):
         result = next(self.pulse_generator)
 
-        eq_((255,0,0), result)
+        eq_((255, 0, 0), result)
 
     def test_mid_phase_is_zero(self):
+        result = None
         for i in range(6):
             result = next(self.pulse_generator)
 
-        eq_((0,0,0), result)
+        eq_((0, 0, 0), result)
 
     def test_full_phase_us_target(self):
+        result = None
         for i in range(11):
             result = next(self.pulse_generator)
 
-        eq_((255,0,0), result)
+        eq_((255, 0, 0), result)
